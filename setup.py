4c0a37a5fdc4c26771fd7b6329b9847775dded55
barryguda-1 <52743757+barryguda-1@users.noreply.github.com>
2019-09-05 13:22:29 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+
+setup(
+        name='pgbackup',
+        version='0.1.0',
+        author='Barry Guda',
+        author_email='barry.guda01@gmail.com',
+        description='A utility for backing up PostgreSql databases',
+        long_description = long_description,
+        long_description_content_type = 'text/markdown',
+        url = 'https://github.com/barryguda-1/pgbackup',
+        packages=find_packages('src'),
+        package_dir={'': 'src'},
+        install_requires=['boto3'],
+        python_requires='>=3.6',
+        entry_points={
+            'console_scripts': [
+                    'pgbackup=pgbackup.cli:main',
+                ],
+        }
+ )
